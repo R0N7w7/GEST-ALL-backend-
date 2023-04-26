16bdc5397c5da90df3cf803029e818a98bf6f057
Roberto Cruz <cr439694@gmail.com>
2023-04-26 17:36:49 -0600

    Arreglo controlador empleado para buscar por campo

--- controllers/empleado.controller.js
+++ controllers/empleado.controller.js
@@ -18,41 +18,45 @@ export const listarEmpleados = async (req, res) => {
 //Lista los empleados que cumplen con un campo en especifico
 export const buscarEmpleados = async (req, res) => {
     const { campo, valor } = req.params;
+    try {
+        let atributo;
+        console.log(campo);
+        switch (campo) {
+            case 'id_empleado':
+                atributo = "id_empleado"
+                break;
+            case 'nombre':
+                atributo = 'nombre';
+                break;
+            case 'apellido_materno':
+                atributo = 'apellido_materno';
+                break;
+            case 'apellido_paterno':
+                atributo = 'apellido_paterno';
+                break;
+            case 'telefono':
+                atributo = 'telefono';
+                break;
+            default:
+                return res.status(400).json({ message: 'Campo no Válido' });
 
-    let atributo;
-    console.log(campo);
-    switch (campo) {
-        case 'id_empleado':
-            atributo = "id_empleado"
-            break;
-        case 'nombre':
-            atributo = 'nombre';
-            break;
-        case 'apellido_materno':
-            atributo = 'apellido_materno';
-            break;
-        case 'apellido_paterno':
-            atributo = 'apellido_paterno';
-            break;
-        case 'telefono':
-            atributo = 'telefono';
-            break;
-        default:
-            return res.status(400).json({ message: 'Campo no Válido' });
-
-    }
-    const empleados = await Empleado.findAll({
-        where: {
-            [atributo]: {
-                [Op.like]: `%${valor}%`
+        }
+        const empleados = await Empleado.findAll({
+            where: {
+                [atributo]: {
+                    [Op.like]: `%${valor}%`
+                }
             }
         }
+        );
+        if (empleados.length <= 0) {
+            return res.json({ });
+        }
+        res.json({ empleados });
+    } catch (error) {
+        return res.status(500).json({ message: "Error al consultar" });
     }
-    );
-    if (empleados.length <= 0) {
-        return res.status(404).json({ message: "Empleado no encontrado" });
-    }
-    res.json({ empleados });
+
 }
 
 //Crea un nuevo empleado
@@ -101,28 +105,28 @@ export const eliminarEmpleado = async (req, res) => {
 export const actualizarEmpleado = async (req, res) => {
     const { id_empleado } = req.params;
     const { nombre, apellido_paterno, apellido_materno, telefono, salario_hora, salario_hora_extra } = req.body;
-  
+
     try {
-      const empleado = await Empleado.findOne({ where: { id_empleado } });
-  
-      if (!empleado) {
-        return res.status(404).json({ message: "Empleado no encontrado" });
-      }
-  
-      empleado.nombre = nombre;
-      empleado.apellido_paterno = apellido_paterno;
-      empleado.apellido_materno = apellido_materno;
-      empleado.telefono = telefono;
-      empleado.salario_hora = salario_hora;
-      empleado.salario_hora_extra = salario_hora_extra;
-  
-      await empleado.save();
-  
-      res.json({ message: "Empleado actualizado", empleado });
+        const empleado = await Empleado.findOne({ where: { id_empleado } });
+
+        if (!empleado) {
+            return res.status(404).json({ message: "Empleado no encontrado" });
+        }
+
+        empleado.nombre = nombre;
+        empleado.apellido_paterno = apellido_paterno;
+        empleado.apellido_materno = apellido_materno;
+        empleado.telefono = telefono;
+        empleado.salario_hora = salario_hora;
+        empleado.salario_hora_extra = salario_hora_extra;
+
+        await empleado.save();
+
+        res.json({ message: "Empleado actualizado", empleado });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error al actualizar el empleado" });
+        console.error(error);
+        res.status(500).json({ message: "Error al actualizar el empleado" });
     }
-  };
-  
+};
+
 
